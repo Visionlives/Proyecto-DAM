7a336953051e239bc8e6d69d2f55a94867307f15
Andrey Aranguiz <laques11@gmail.com>
2025-11-24 21:51:35 -0300

    Prueba

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:proyecto_dam/auth_wrapper.dart';
-// import 'package:flutter_firebase/auth_wrapper.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
